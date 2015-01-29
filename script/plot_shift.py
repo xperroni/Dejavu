e418023dae4f3b7bc43a56a6b063c572a8959d30
helio <helio@roboken.iit.tsukuba.ac.jp>
2015-01-29 18:49:07 +0900

    Frame number synchronization across data files
    Signed-off-by: helio <helio@roboken.iit.tsukuba.ac.jp>

--- script/plot_shift.py
+++ script/plot_shift.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from itertools import count, izip
 from math import exp
+from os.path import join as joinpath
 from sys import float_info
 
 from matplotlib import pyplot, cm
@@ -9,7 +11,7 @@ from numpy import array, arange
 from numpy import min as amin
 from numpy import max as amax
 from numpy import sum as asum
-from numpy import argmax
+from numpy import argmin, argmax
 
 
 def gaussian(x, sigma):
@@ -18,13 +20,15 @@ def gaussian(x, sigma):
     return exp(-p / q)
 
 
-def attraction(y, g):
-    #return argmax([g[i] * gaussian(i - y, 25.0) for i in range(0, len(g))])
+#def attraction(y, g):
+#    return argmax([g[i] * gaussian(i - y, 25.0) for i in range(0, len(g))])
 
+
+def attraction(y, g):
     sum_iw = 0.0
-    sum_w = 0.0
+    sum_w = 1.0
     for i in range(0, len(g)):
-        w = g[i] * gaussian(i - y, 2.0)
+        w = g[i] * gaussian(i - y, 1.0)
         sum_iw += i * w
         sum_w += w
 
@@ -46,9 +50,9 @@ def plot_contours(plotter, data, x0):
     pyplot.colorbar(c)
 
 
-def load_data(path, x0):
-    def load_rows(path):
-        for line in open(path):
+def load_data(folder, x0):
+    def load_rows(folder):
+        for line in open(joinpath(folder, 'shifts.txt')):
             row = array(eval(line))
             n = amax(row)
             if n > 0:
@@ -61,33 +65,39 @@ def load_data(path, x0):
             row = asum(array(rows[a:b]), 0)
             yield row
 
-    rows = [row for row in load_rows(path)]
-    return array([row for row in merge(rows)])
+    rows = [row for row in load_rows(folder)]
+    indices = [int(line) for line in open(joinpath(folder, 'indices.txt'))]
+
+    return (array([row for row in merge(rows)]), indices[x0:])
 
 
-def plot_ground(plotter, ground, x0, xn):
-    if ground == '':
+def plot_ground(plotter, path, x0, indices):
+    if path == '':
         return
 
+    def load_ground(path):
+        (x, y) = ([], [])
+        for line in open(path):
+            (x_replay, x_teach, shift) = eval(line)
+            x.append(x_replay)
+            y.append(shift / 12.0)
+
+        return (array(x), array(y))
+
+    (u, v) = load_ground(path)
     (x, y) = ([], [])
-    for line in open(ground):
-        (x_replay, x_teach, shift) = eval(line)
-        if x_replay < x0:
-            continue
+    for (i, k) in izip(indices, count(x0)):
+        x.append(k)
+        j = argmin((u - i) ** 2.0)
+        y.append(v[j])
 
-        if x_replay >= xn:
-            break
-
-        x.append(x_replay)
-        y.append(shift)
-
-    plotter.plot(x, y, 'w--')
+    plotter.plot(x, y, 'k--')
 
 
 
-def plot(x0, path, ground=''):
+def plot(x0, folder, ground=''):
     x0 = int(x0)
-    data = load_data(path, x0)
+    (data, indices) = load_data(folder, x0)
     (n, m) = data.shape
 
     (figure, axes) = pyplot.subplots()
@@ -95,21 +105,25 @@ def plot(x0, path, ground=''):
 
     d = shifts(data)
     xn = x0 + len(d)
-    print xn
 
     x = range(x0, xn)
     z = (n - x0)
-    axes.plot(x, d, 'k--')
+    axes.plot(x, d, 'k-')
 
-    plot_ground(axes, ground, x0, xn)
+    plot_ground(axes, ground, x0, indices)
 
     axes.axis([x0, n, -m // 2, m // 2])
     axes.xaxis.set_ticks_position('bottom')
     axes.set_aspect('auto', 'box')
 
+    ticks = arange(x0, n, 50.0)
+    axes.set_xticks(ticks)
+    axes.set_xticklabels([str(indices[int(i - x0)]) for i in ticks])
+
+
     axes.grid()
     axes.set_xlabel('Replay image index #', labelpad=10)
-    axes.set_ylabel('Shift (pixels)', labelpad=20)
+    axes.set_ylabel('Shift (columns)', labelpad=20)
 
     pyplot.show()
 
